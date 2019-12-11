dd2a30ae0a800c56ab98a06117ce393e67ffe0aa
Sachin Gurung <saching061@hotmail.com>
2019-12-10 22:51:38 -0800

    First commit

--- Separate characters from string/Program.cs
+++ Separate characters from string/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Separate_characters_from_string
+public class Exercise3
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string str;
+        int l = 0;
+
+        Console.Write("\n\nSeparate the individual characters from a string :\n");
+        Console.Write("------------------------------------------------------\n");
+        Console.Write("Input the string : ");
+        str = Console.ReadLine();
+        Console.Write("The characters of the string are  :  ");
+        while (l <= str.Length - 1)
         {
+            Console.Write("{0} ", str[l]);
+            l++;
         }
+        Console.Write("\n\n");
     }
 }
+/*
+Separate the individual characters from a string :
+------------------------------------------------------
+Input the string : sachin gurung
+The characters of the string are  :  s a c h i n   g u r u n g
+
+Press any key to continue . . .
+*/
